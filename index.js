b4271a276f5813a51a31ffe41970fab97ec2f07c
Darwing <darwingx990@hotmail.com>
2025-11-13 06:57:12 -0500

    COunter created and Schema to AutoIncrement the Id

--- index.js
+++ index.js
@@ -7,3 +7,26 @@ const userTypeRoutes = require("./routes/userTypeRoutes");
 // Middlewares 
 app.use(cors())
 app.use(XPathExpression.json());
+app.use(XPathExpression.urlencoded({ extended: true }));
+
+// Routes of the API
+app.use("/api/user-type", userTypeRoutes);
+
+// Root ROute
+app.get("/", (req, res) => {
+    res.json({
+        success: true,
+        message: "My first API with NodeJS and Express. It is working correctly!",
+        version: "1.0",
+        endpoints: {
+            userType: {
+                base: "/api/user-type",
+                endpoints: {
+                    "POST"
+                }
+            }
+        }
+    })
+})
+
+
--- models/UserType.js
+++ models/UserType.js
@@ -1,6 +1,6 @@
 const { Collection } = require("mongoose");
 
-// Creating the counter schema and model
+// Creating the counter schema
 const counterSchema = new mongoose.Schema({
     _id: { type: String, required: true },
     sequence: { type: Number, default: 0 }
@@ -9,9 +9,26 @@ const Counter = mongoose.model('Counter', counterSchema) || mongoose.model.Count
 
 // Creating the UserType schema
 const userTypeSchema = new mongoose.Schema({
-    userTypeId: { type: Number, unique: true },
-    description: { type: String, required: true}
+    userTypeId: { type: Number, unique: true, unique: true },
+    description: { type: String, required: true, trim: true }
 }, {
     timestamps: true,
-    Collection: "UserType"    
+    Collection: "UserType" // Specify and force the collection name in the DB.
+})
+
+// Pre-save hook to auto-increment userTypeId
+userTypeSchema.pre("save", async function (next) {
+    if (this.isNew) {
+        try {
+            const counter = await Counter.findByIdAndUpdate(
+                userTypeId,
+                { $inc: { sequence: 1 } },
+                { new: true, upsert: true }
+            );
+            this.userTypeId = counter.sequence;
+        } catch (error) {
+            return next(error);
+        }
+    }
+    next();
 })
